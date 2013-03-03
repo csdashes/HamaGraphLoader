a274080311649d0d90af126c86f1feafa815ef65
Anastasis Andronidis <anastasis90@yahoo.gr>
2013-03-03 19:20:44 +0200

    Init of project

---
+++ src/hamagraphloader/HamaGraphLoader.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hamagraphloader;
+
+/**
+ *
+ * @author Anastasis Andronidis <anastasis90@yahoo.gr>
+ */
+public class HamaGraphLoader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
